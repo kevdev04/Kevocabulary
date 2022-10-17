8d39e0f8d51891f30dfa8599b10d2f3c8f40d3d9
kevdev_p <kggc03@gmail.com>
2022-10-16 22:44:54 -0500

    come back

--- js/en.js
+++ js/en.js
@@ -1,42 +1,50 @@
-const wordSelect = document.querySelector('.first');
-const wordPut = document.createElement('h2');
-        wordPut.textContent = 'whever'
+document.addEventListener('DOMContentLoaded', function () {
 
 
-const btnReload = document.querySelector('.button')
-btnReload.addEventListener('click', () => {
-        
-});
+// Aqui andan las variables
+const word = document.querySelector('#word');
+const palabra = document.querySelector('#palabra');
+const btnReload = document.querySelector('#button');
 
 
+btnReload.addEventListener('click', reload);
+
+function reload (e) {
+
+        let resultado;
+
+        resultado = 'helpme'
+
+        console.table(words);
+
+
+
+        word.textContent = resultado;
+        palabra.textContent = resultado;
+}
 
 
 
 const words = [
 
 {
-	word: "the",
-        palabra: "El / Los",
-        sentence: "The sun is up",
+	word: "the",palabra: "El / Los",sentence: "The sun is up",
         oracion: "El sol ha salido"
 },
 {
-	word: "be",
-        palabra: "Ser / Estar",
-        sentence: "Don't be sad",
+	word: "be",palabra: "Ser / Estar",sentence: "Don't be sad",
         oracion: "No estes trsite"
 },
 {
-        word: 'as',
-        palabra: 'como (adv.)',
-        sentence: 'He used me as a guinea pig',
+        word: 'as',palabra: 'como (adv.)',sentence: 'He used me as a guinea pig',
         oracion: 'Me utilizó como conejillo de indias'
 },
 {
-        word: 'will',
-        palabra: '(futuro), voluntad',
-        sentence: 'Will she come?',
+        word: 'will', palabra: '(futuro), voluntad', sentence: 'Will she come?',
         oracion: '¿Vendrá? (Ella)'
 }
 
 ];
+
+
+});
