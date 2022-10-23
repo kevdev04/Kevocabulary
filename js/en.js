84c24237ac08de4fa82a9f8902381606ca8b3b49
kevdev <kggc03@gmail.com>
2022-10-22 22:22:54 -0500

    casi

--- js/en.js
+++ js/en.js
@@ -2,46 +2,54 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 // Aqui andan las variables
-const word = document.querySelector('#word');
-const palabra = document.querySelector('#palabra');
+const wordy = document.querySelector('#word');
+const palabraHTML = document.querySelector('#palabra');
+const sentence = document.querySelector('#sen')
+const oracion = document.querySelector('#oracion')
 const btnReload = document.querySelector('#button');
 
 
 btnReload.addEventListener('click', reload);
 
-function reload (e) {
+function reload () {
 
-        let resultado = words.map(e => e.word1);
+        const conjunto = words.map((word) => word.word);
+        const conjunto2 = words.map((word) => word.palabra);
+        const conjunto3 = words.map((word) => word.sentence);
+        const conjunto4 = words.map((word) => word.oracion);
 
-        let resultado2 = words.map(e => e.palabra1);
+        
 
-        console.log(resultado);
-
-
-
-        word.textContent = resultado;
-        palabra.textContent = resultado2;
+        wordy.textContent = conjunto[0]
+        palabraHTML.textContent = conjunto2[0]
+        sentence.textContent = conjunto3[0]
+        oracion.textContent = conjunto4[0]
 }
-
+btnReload.addEventListener('click', magia2);
+function magia2() {
+        if (conjunto[i] === 0) {
+                conjunto[i++]
+        }
+}
 
 
 const words = [
 
 {
-	word1: "the",
-        palabra1: "El / Los",sentence: "The sun is up",
+	word: "the",
+        palabra: "El / Los",sentence: "The sun is up",
         oracion: "El sol ha salido"
 },
 {
-	word2: "be",palabra: "Ser / Estar",sentence: "Don't be sad",
+	word: "be",palabra: "Ser / Estar",sentence: "Don't be sad",
         oracion: "No estes trsite"
 },
 {
-        word3: 'as',palabra: 'como (adv.)',sentence: 'He used me as a guinea pig',
+        word: 'as',palabra: 'como (adv.)',sentence: 'He used me as a guinea pig',
         oracion: 'Me utilizó como conejillo de indias'
 },
 {
-        word4: 'will', palabra: '(futuro), voluntad', sentence: 'Will she come?',
+        word: 'will', palabra: '(futuro), voluntad', sentence: 'Will she come?',
         oracion: '¿Vendrá? (Ella)'
 }
 
