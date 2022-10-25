91a1456e801fbc96cb2f04797ffdba9d9190cc6a
kevdev_p <kggc03@gmail.com>
2022-10-25 10:34:52 -0500

    add more db

--- js/verbs.js
+++ js/verbs.js
@@ -43,23 +43,74 @@ function reload() {
 const verbs = [
 
 {
-        verb: 'avoid',
+        verb: 'Avoid',
         verbo: 'evitar',
         sentence: 'She took a detour to avoid the heavy traffic',
         oracion: 'Tomó un desvío para evitar el tráfico pesado'
 },
 {
-        verb: 'waste',
+        verb: 'Waste',
         verbo: 'desperdiciar',
         sentence: 'Dont waste your money.',
         oracion: 'No desperdicies tu dinero.'
 },
 {
-        verb: 'lay',
+        verb: 'Lay',
         verbo: 'poner',
         sentence: ' I dont like to lay my purse on the floor.',
         oracion: 'No me gusta dejar mi bolso en el suelo.'
 },
+{
+        word: 'Become',
+        palabra: 'convertirse en / convertirse en',
+        sentence: 'Mary wants to become a teacher.',
+        oracion: 'María quiere ser profesora.'
+},
+{
+        word: 'halt',
+        palabra: 'detener',
+        sentence: 'The project had to be halted due to lack of funds.',
+        oracion: 'El proyecto tuvo que detenerse por falta de fondos.'
+},
+{
+        word: 'stood',
+        palabra: 'puso',
+        sentence: 'He stood on the edge of the cliff.',
+        oracion: 'Se puso al borde del acantilado.'
+},
+{
+        word: 'rest',
+        palabra: 'descansar',
+        sentence: 'His head was resting on her shoulder.',
+        oracion: 'Su cabeza estaba apoyada en su hombro.'
+},
+{
+        word: 'might',
+        palabra: 'puede',
+        sentence: 'I might see you tomorrow.',
+        oracion: 'Puede que te vea mañana.'
+},
+{
+        word: 'thought',
+        palabra: 'pensamiento',
+        sentence: 'I thought we were going to go somewhere.',
+        oracion: 'Pensé que íbamos a ir a algún sitio.'
+},
+{
+        word: 'filled',
+        palabra: 'rellenado',
+        sentence: 'He filled the bucket with water',
+        oracion: 'Llenó el cubo de agua'
+},
+{
+        word: 'matter',
+        palabra: 'asunto / importancia ',
+        sentence: 'No matter how much she eats, she never gains weight.  I dont think he has anything to do with the matter.',
+        oracion: 'No importa cuánto coma, nunca gana peso. No creo que tenga nada que ver con el asunto. '
+},
+{
+
+}
 
 ];
 
--- js/words.js
+++ js/words.js
@@ -44,17 +44,62 @@ const words = [
                 oracion: 'Tom es muy amable'
         },
         {
-                word: 'thought',
-                palabra: 'pensamiento',
-                sentence: 'I thought we were going to go somewhere.',
-                oracion: 'Pensé que íbamos a ir a algún sitio.'
+                word: 'surface',
+                palabra: 'superficie',
+                sentence: 'Birds skimmed the surface of the pond.',
+                oracion: 'Los pájaros rozaban la superficie del estanque.'
         },
         {
-                word: 'might',
-                palabra: 'puede',
-                sentence: 'I might see you tomorrow.',
-                oracion: 'Puede que te vea mañana.'
-        }
+                word: 'measure',
+                palabra: 'medida',
+                sentence: 'We want to measure your blood pressure.',
+                oracion: 'Queremos medir su presión arterial.'
+        },
+        {
+                word: 'whole',
+                palabra: 'todo / toda',
+                sentence: 'We saw the whole family at the reunion.',
+                oracion: 'Vimos a toda la familia en la reunión.'
+        },
+        {
+                word: 'Toward',
+                palabra: 'Hacia',
+                sentence: 'The river runs toward the sea.',
+                oracion: 'El río corre hacia el mar.'
+        },
+        {
+                word: 'upon',
+                palabra: 'Sobre / En',
+                sentence: 'He carefully placed the vase upon the table. ',
+                oracion: 'Colocó cuidadosamente el jarrón sobre la mesa.'
+        },
+        {
+                word: 'wave',
+                palabra: 'ola / onda',
+                sentence: 'Flags were waving in the breeze.',
+                oracion: 'Las banderas ondeaban en la brisa.'
+        },
+        {
+                word: 'though',
+                palabra: 'aunque',
+                sentence: 'Though I do not usually drink coffee',
+                oracion: 'Aunque no suelo tomar café'
+        },
+        {
+                word: 'shape',
+                palabra: 'forma',
+                sentence: 'The athlete is in perfect shape.',
+                oracion: 'El atleta está en perfecta forma.'
+        },
+        {
+                word: 'noun',
+                palabra: 'sustantivo',
+                sentence: 'person: man, woman, teacher, John, Mary.',
+                oracion: 'place: home, office, town, countryside, America.'
+        },
+        
+        
+
 
 ];
 
