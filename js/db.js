ee21c6133b0253a659a60b84bc4a660352c1ce7c
kevdev_p <kggc03@gmail.com>
2022-10-25 15:31:14 -0500

    v2.3

---
+++ js/db.js
@@ -0,0 +1,227 @@
+
+
+    const verbs = [
+
+        {
+                word: 'Avoid',
+                palabra: 'evitar',
+                sentence: 'She took a detour to avoid the heavy traffic',
+                oracion: 'Tomó un desvío para evitar el tráfico pesado'
+        },
+        {
+                word: 'Waste',
+                palabra: 'desperdiciar',
+                sentence: 'Dont waste your money.',
+                oracion: 'No desperdicies tu dinero.'
+        },
+        {
+                word: 'Lay',
+                palabra: 'poner',
+                sentence: ' I dont like to lay my purse on the floor.',
+                oracion: 'No me gusta dejar mi bolso en el suelo.'
+        },
+        {
+                word: 'Become',
+                palabra: 'convertirse en / convertirse en',
+                sentence: 'Mary wants to become a teacher.',
+                oracion: 'María quiere ser profesora.'
+        },
+        {
+                word: 'halt',
+                palabra: 'detener',
+                sentence: 'The project had to be halted due to lack of funds.',
+                oracion: 'El proyecto tuvo que detenerse por falta de fondos.'
+        },
+        {
+                word: 'stood',
+                palabra: 'puso',
+                sentence: 'He stood on the edge of the cliff.',
+                oracion: 'Se puso al borde del acantilado.'
+        },
+        {
+                word: 'rest',
+                palabra: 'descansar',
+                sentence: 'His head was resting on her shoulder.',
+                oracion: 'Su cabeza estaba apoyada en su hombro.'
+        },
+        {
+                word: 'might',
+                palabra: 'puede',
+                sentence: 'I might see you tomorrow.',
+                oracion: 'Puede que te vea mañana.'
+        },
+        {
+                word: 'thought',
+                palabra: 'pensamiento',
+                sentence: 'I thought we were going to go somewhere.',
+                oracion: 'Pensé que íbamos a ir a algún sitio.'
+        },
+        {
+                word: 'filled',
+                palabra: 'rellenado',
+                sentence: 'He filled the bucket with water',
+                oracion: 'Llenó el cubo de agua'
+        },
+        {
+                word: 'matter',
+                palabra: 'asunto / importancia ',
+                sentence: 'No matter how much she eats, she never gains weight.  I dont think he has anything to do with the matter.',
+                oracion: 'No importa cuánto coma, nunca gana peso. No creo que tenga nada que ver con el asunto. '
+        },
+        
+        ];
+    
+    
+    
+    
+        const words = [
+    
+            {
+                    word: 'kind',
+                    palabra: 'amable',
+                    sentence: 'Tom is very kind',
+                    oracion: 'Tom es muy amable'
+            },
+            {
+                    word: 'surface',
+                    palabra: 'superficie',
+                    sentence: 'Birds skimmed the surface of the pond.',
+                    oracion: 'Los pájaros rozaban la superficie del estanque.'
+            },
+            {
+                    word: 'measure',
+                    palabra: 'medida',
+                    sentence: 'We want to measure your blood pressure.',
+                    oracion: 'Queremos medir su presión arterial.'
+            },
+            {
+                    word: 'whole',
+                    palabra: 'todo / toda',
+                    sentence: 'We saw the whole family at the reunion.',
+                    oracion: 'Vimos a toda la familia en la reunión.'
+            },
+            {
+                    word: 'Toward',
+                    palabra: 'Hacia',
+                    sentence: 'The river runs toward the sea.',
+                    oracion: 'El río corre hacia el mar.'
+            },
+            {
+                    word: 'upon',
+                    palabra: 'Sobre / En',
+                    sentence: 'He carefully placed the vase upon the table. ',
+                    oracion: 'Colocó cuidadosamente el jarrón sobre la mesa.'
+            },
+            {
+                    word: 'wave',
+                    palabra: 'ola / onda',
+                    sentence: 'Flags were waving in the breeze.',
+                    oracion: 'Las banderas ondeaban en la brisa.'
+            },
+            {
+                    word: 'though',
+                    palabra: 'aunque',
+                    sentence: 'Though I do not usually drink coffee',
+                    oracion: 'Aunque no suelo tomar café'
+            },
+            {
+                    word: 'shape',
+                    palabra: 'forma',
+                    sentence: 'The athlete is in perfect shape.',
+                    oracion: 'El atleta está en perfecta forma.'
+            },
+            {
+                    word: 'noun',
+                    palabra: 'sustantivo',
+                    sentence: 'person: man, woman, teacher, John, Mary.',
+                    oracion: 'place: home, office, town, countryside, America.'
+            },
+            {
+                    word: 'perhaps',
+                    palabra: 'quizas',
+                    sentence: '',
+                    oracion: ''
+            },
+            {
+                    word: 'suddenly',
+                    palabra: 'de repente',
+                    sentence: '',
+                    oracion: ''
+            },
+            {
+                    word: 'wide',
+                    palabra: 'ancho',
+                    sentence: '',
+                    oracion: ''
+            },
+            
+    
+    ];
+    
+    
+    
+    
+        const pv = [
+    
+        {
+                word: 'Catch out',
+                palabra: 'Atrapado',
+                sentence: 'One day he caught her out when he found some letters shed written.',
+                oracion: 'Un día la sorprendió cuando encontró unas cartas que ella había escrito.'
+        },
+        {
+                word: 'Catch on',
+                palabra: 'Póngase al día',
+                sentence: 'This game will catch on among young people.',
+                oracion: 'Este juego se pondrá de moda entre los jóvenes.'
+        },
+        {
+                word: 'Catch up',
+                palabra: 'Alcanzar',
+                sentence: 'John caught up with you later.',
+                oracion: 'John te alcanzo más tarde.'
+        },
+        {
+                word: 'Clear away',
+                palabra: 'Recoger / Despejar',
+                sentence: 'You should clear away all your toys before bedtime.',
+                oracion: 'Debes recoger todos sus juguetes antes de acostarse.'
+        },
+        {
+                word: 'Clear up',
+                palabra: 'Aclarar',
+                sentence: ' I think the trouble will clear up in a couple of days.',
+                oracion: 'Creo que el problema se aclarará en un par de días.'
+        },
+        {
+                word: 'Check off',
+                palabra: 'Marque la casilla',
+                sentence: 'Let’s check off the names of the people to be invited.',
+                oracion: 'Marquemos los nombres de las personas a las que hay que invitar.'
+        },
+        {
+                word: 'Knock off',
+                palabra: '',
+                sentence: 'What time will they knock off?',
+                oracion: '¿A qué hora van a salir?'
+        },
+        {
+                word: 'Carry over',
+                palabra: 'Traspaso',
+                sentence: 'The meeting carried over into lunch time.',
+                oracion: 'La reunión se prolongó hasta la hora del almuerzo.'
+        },
+        {
+                word: 'Get on',
+                palabra: 'subir',
+                sentence: 'I think they get on the wrong train.',
+                oracion: 'Creo que se suben al tren equivocado.'
+        },
+        {
+                word: 'Drop off',
+                palabra: 'dejar',
+                sentence: 'Where do you want me to drop you off?',
+                oracion: '¿Dónde quieres que te deje?'
+        }
+    
+    ];
